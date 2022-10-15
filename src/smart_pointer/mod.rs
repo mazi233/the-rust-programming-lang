4db19955afe1b008110e1c193efeb70ea37e7c2c
mazi <602281666@qq.com>
2022-10-15 21:22:37 +0800

    commit

--- src/smart_pointer/mod.rs
+++ src/smart_pointer/mod.rs
@@ -1,3 +1,4 @@
+use std::rc::Weak;
 use std::{ops::Deref, rc::Rc};
 use std::cell::RefCell;
 
@@ -66,6 +67,56 @@ pub fn main() {
   println!("a after = {:?}", a);
   println!("b after = {:?}", b);
   println!("c after = {:?}", c);
+
+
+  let leaf = Rc::new(Node {
+    value: 3,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![]),
+  });
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  let branch = Rc::new(Node {
+    value: 5,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![Rc::clone(&leaf)]),
+  });
+  *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  
+  let leaf = Rc::new(Node {
+    value: 3,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![]),
+  });
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
+  {
+    let branch = Rc::new(Node {
+      value: 5,
+      parent: RefCell::new(Weak::new()),
+      children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!(
+      "branch strong = {}, weak = {}",
+      Rc::strong_count(&branch),
+      Rc::weak_count(&branch),
+    );
+    println!(
+      "leaf strong = {}, weak = {}",
+      Rc::strong_count(&leaf),
+      Rc::weak_count(&leaf),
+    );
+  }
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  println!(
+    "leaf strong = {}, weak = {}",
+    Rc::strong_count(&leaf),
+    Rc::weak_count(&leaf),
+  );
 }
 
 #[derive(Debug)]
@@ -144,6 +195,13 @@ where
         .send("Warning: You've used up over 75% of your quota!");
     }
   }
+}
+
+#[derive(Debug)]
+struct Node {
+  value: i32,
+  parent: RefCell<Weak<Node>>,
+  children: RefCell<Vec<Rc<Node>>>,
 }
 
 #[cfg(test)]
